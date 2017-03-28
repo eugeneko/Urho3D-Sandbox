739eb70aa1f47178ed59f7475fbce7442c2a2da3
Eugene Kozlov <nekoeuge@gmail.com>
2017-03-28 10:02:20 +0300

    Add test model and scene. Remove unneeded animations.

--- bin/Examples/CharacterAnimator/BakeAnimations.bat
+++
@@ -1,4 +0,0 @@
-..\..\CharacterAnimationTool.exe Swat.mdl Swat_Skeleton.xml Swat_WalkFwd.ani Swat_WalkFwd.xml
-..\..\CharacterAnimationTool.exe Swat.mdl Swat_Skeleton.xml Swat_WalkBwd.ani Swat_WalkBwd.xml
-..\..\CharacterAnimationTool.exe Swat.mdl Swat_Skeleton.xml Swat_WalkLeft.ani Swat_WalkLeft.xml
-..\..\CharacterAnimationTool.exe Swat.mdl Swat_Skeleton.xml Swat_WalkRight.ani Swat_WalkRight.xml
